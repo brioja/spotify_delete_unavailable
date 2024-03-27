1407c5a9644b607e89e6ed67d7de610275c6e44a
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2024-03-27 13:47:35 -0400

    feat: basic app for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import RedirectPage from './RedirectPage';
 
 function App() {
   return (
-    <Router basename="/spotify_delete_unavailable">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/redirect" element={<RedirectPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is a basic setup without routing.</p>
+    </div>
   );
 }
 
---
+++ src/App_route.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import RedirectPage from './RedirectPage';
+
+function App() {
+  return (
+    <Router basename="/spotify_delete_unavailable">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/redirect" element={<RedirectPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
