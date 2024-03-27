597afcecd52a3178eea10b36ab11e3c679f5d516
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2024-03-27 14:14:54 -0400

    feat: hash routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import RedirectPage from './RedirectPage';
 
 function App() {
   return (
-    <Router basename="/spotify_delete_unavailable">
+    <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/redirect" element={<RedirectPage />} />
