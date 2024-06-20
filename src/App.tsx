151e492977ed5a60350ea2852a75b9cc0717d931
Brandon Rioja <34580058+brioja@users.noreply.github.com>
2024-06-20 10:01:56 -0400

    feat: success page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
-import RedirectPage from './RedirectPage';
+import SuccessPage from './SuccessPage';
 
 function App() {
     return (
          <Router basename="/spotify_delete_unavailable">
             <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/redirect" element={<RedirectPage />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/success" element={<SuccessPage />} />
       </Routes>
     </Router>
   );
--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const clientId = 'bb5d13f3082c499fa9a2b2f86297b91d';
 
-// Dynamically set redirectUri based on the environment
+// Dynamically set redirectUri based on the environment                                                                                                                                 
 const redirectUri = process.env.NODE_ENV === 'production'
   ? encodeURIComponent('https://brioja.github.io/spotify_delete_unavailable/')
   : encodeURIComponent('http://localhost:3000/spotify_delete_unavailable/');
@@ -14,19 +14,22 @@ const showDialog = true;
 const spotifyLoginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}&show_dialog=${showDialog}`;
 
 const LoginPage: React.FC = () => {
-    const location = useLocation();
-    const code = new URLSearchParams(location.search).get('code');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const code = new URLSearchParams(location.search).get('code');
 
-    return (
+  useEffect(() => {
+    if (code) {
+      navigate('/success', { state: { code } });
+    }
+  }, [code, navigate]);
+
+  return (
     <div>
       <h1>Login with Spotify</h1>
       <a href={spotifyLoginUrl}>Log In</a>
-
-      <h1>Login Successful</h1>
-      <p>Your authorization code is: {code}</p>
     </div>
   );
-    
 };
 
 export default LoginPage;
